f958251f8b47cfff0f0657b21432ab08f462206a
Tudor <ionelpaul2001@gmail.com>
2023-09-20 15:16:54 +0300

    first commit

---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'app-property-card',
+    templateUrl:'property-card.component.html',
+    styleUrls:['property-card.component.css']
+}
+
+)
+
+export class PropertyCardComponent {
+    Property: any={
+        "ID":1,
+        "Name":"Casa Smechera",
+        "Type":"House",
+        "Price":100000
+    }
+}
